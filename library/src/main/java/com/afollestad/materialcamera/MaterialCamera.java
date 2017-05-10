a7251778a7ab259375683a58ea3b002bd6117881
martin gordon <mg.martingordon@gmail.com>
2017-05-10 16:35:45 +0300

    - Use always CaptureActivity

--- library/src/main/java/com/afollestad/materialcamera/MaterialCamera.java
+++ library/src/main/java/com/afollestad/materialcamera/MaterialCamera.java
@@ -318,7 +318,7 @@ public class MaterialCamera {
 
     public Intent getIntent() {
         final Class<?> cls = !mForceCamera1 && CameraUtil.hasCamera2(mContext, mStillShot) ?
-                CaptureActivity2.class : CaptureActivity.class;
+                CaptureActivity.class : CaptureActivity.class;
         Intent intent = new Intent(mContext, cls)
                 .putExtra(CameraIntentKey.LENGTH_LIMIT, mLengthLimit)
                 .putExtra(CameraIntentKey.ALLOW_RETRY, mAllowRetry)
